8cc05280eb10fb17e0a396f744fb682714a53ead
Dipinto <Diego_Pinto@gap.com>
2021-03-26 17:00:17 -0300

    testContainer for component test

--- acceptance-test/src/test/java/com/exmple/demo/acceptance/common/stepdefs/AbstractDefs.java
+++ acceptance-test/src/test/java/com/exmple/demo/acceptance/common/stepdefs/AbstractDefs.java
@@ -3,13 +3,17 @@ package com.exmple.demo.acceptance.common.stepdefs;
 import com.example.demo.DemoApplication;
 import com.exmple.demo.acceptance.kafka.KafkaConfiguration;
 import io.cucumber.spring.CucumberContextConfiguration;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @CucumberContextConfiguration
 @SpringBootTest(value = "sever.port=8081", webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ContextConfiguration(classes = {DemoApplication.class, KafkaConfiguration.class},
         loader = SpringBootContextLoader.class)
+@RunWith(SpringRunner.class)
 public abstract class AbstractDefs {
+
 }
--- acceptance-test/src/test/java/com/exmple/demo/acceptance/kafka/KafkaConfiguration.java
+++ acceptance-test/src/test/java/com/exmple/demo/acceptance/kafka/KafkaConfiguration.java
@@ -1,13 +1,27 @@
 package com.exmple.demo.acceptance.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.DefaultKafkaProducerFactoryCustomizer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.Collections;
 
 @Configuration
 public class KafkaConfiguration {
+
+    @Autowired
+    private KafkaProperties properties;
+
     @Autowired
     private KafkaMessageCache<String> stringMessage;
 
@@ -19,6 +33,31 @@ public class KafkaConfiguration {
     @KafkaListener(topics = "#{'${io.confluent.developer.config.topic.name}'}")
     public void consume(final ConsumerRecord<String, String> consumerRecord) {
         stringMessage.addMessage(consumerRecord.value());
+    }
+
+    @Bean
+    public KafkaContainer kafkaContainer() {
+        KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.4.3"));
+        kafka.withEmbeddedZookeeper();
+        kafka.start();
+        String host = kafka.getBootstrapServers();
+        properties.setBootstrapServers(Collections.singletonList(host));
+        System.out.println("KAFKA running in host " + host);
+        return kafka;
+    }
+
+    @DependsOn("kafkaContainer")
+    @Bean
+    public ProducerFactory<?, ?> kafkaProducerFactory(
+            ObjectProvider<DefaultKafkaProducerFactoryCustomizer> customizers) {
+        DefaultKafkaProducerFactory<?, ?> factory = new DefaultKafkaProducerFactory<>(
+                this.properties.buildProducerProperties());
+        String transactionIdPrefix = this.properties.getProducer().getTransactionIdPrefix();
+        if (transactionIdPrefix != null) {
+            factory.setTransactionIdPrefix(transactionIdPrefix);
+        }
+        customizers.orderedStream().forEach((customizer) -> customizer.customize(factory));
+        return factory;
     }
 
 }
